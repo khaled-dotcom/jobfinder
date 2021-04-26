0e013cdf38692e6aa0bf0b3caddb6ef2456734cb
khaled-dotcom <68504619+khaled-dotcom@users.noreply.github.com>
2021-04-26 12:30:34 +0100

    updated home_screen

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -5,98 +5,119 @@ import 'package:job1/screens/home_screen/widgets/job_catagory.dart';
 import 'package:job1/screens/home_screen/widgets/popular_job.dart';
 import 'package:job1/screens/home_screen/widgets/search_field.dart';
 
-
-
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-          child: Container(
-            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                //appbar
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //appbar
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  //menu icon
+
+                  GestureDetector(
+                    child: Container(
                       height: 40,
                       width: 40,
                       child: Image.asset(
                         "assets/icons/doticon.png",
                       ),
                       decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(20.0)
-                      ),
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20.0)),
                     ),
-                    Container(
-                        height: 40,
-                        width: 40,
+                    onTap: () {},
+                  ),
+
+                  //profile
+
+                  GestureDetector(
+                    child: Container(
+                      height: 40,
+                      width: 40,
                       padding: EdgeInsets.all(5.0),
                       child: Image.asset("assets/images/person2.png"),
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(20.0),
                         color: Colors.deepOrange,
                       ),
-                      
+                    ),
+                    onTap: () {},
+                  )
+
+                  //profile end
+                ],
+              ),
+
+              // welcome part
+              Padding(
+                padding: EdgeInsets.only(top: 30, bottom: 30),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Welcome",
+                        style: TextStyle(
+                          color: Colors.black54,
+                        )),
+                    Text(
+                      "Find Your Dream Job!",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold),
                     )
                   ],
                 ),
-                Padding(
-                  padding: EdgeInsets.only( top: 30,bottom: 30),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("Welcome",
-                          style: TextStyle(
-                            color: Colors.black54,
-                          )),
-                      Text(
-                        "Find Your Dream Job!",
-                        style: TextStyle(color: Colors.black, fontSize: 20.0,fontWeight: FontWeight.bold),
-                      )
-                    ],
-                  ),
-                ),
-                SearchField(),
-                SizedBox(height: 20,),
-                Banners(),
-                SizedBox(height: 20,),
-               JobCatagory(),
-                SizedBox(
-                  height: 30,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Popular Job",
-                        style: TextStyle(color: Colors.black, fontSize: 20.0,fontWeight: FontWeight.bold)),
-                    Text("See All")
-                  ],
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                PopularJob()
+              ),
 
-              ],
-            ),
+              SearchField(), //searchbar
+              SizedBox(
+                height: 20,
+              ),
+
+              Banners(), //banner
+              SizedBox(
+                height: 20,
+              ),
+
+              JobCatagory(), //job catagory
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Popular Job",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold)),
+                  Text("See All")
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+
+              PopularJob() //job offers
+            ],
           ),
         ),
-        bottomNavigationBar: BottomNavBar(),
+      ),
 
-
+      //bittim navigation bar
+      bottomNavigationBar: BottomNavBar(),
     );
   }
-
-
 }
